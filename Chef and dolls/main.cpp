67623380f677c1b026925212dac90d40721d3274
sejall <33982474+sejall@users.noreply.github.com>
2018-10-09 17:56:52 +0530

    Add files via upload

---
+++ Chef and dolls/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a;
+        for(i=0;i<n;i++)
+            cin>>a;
+
+    }
+    return 0;
+}
